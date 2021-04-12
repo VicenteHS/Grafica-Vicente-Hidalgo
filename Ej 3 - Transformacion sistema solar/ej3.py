d55f1fe6759f9fcdcafbcd1738e351f95b67a39a
VicenteHS <vicente.hidalgo@ug.uchile.cl>
2021-04-12 18:33:11 -0400

    Update ej3.py
    sigo intentando

--- Ej 3 - Transformacion sistema solar/ej3.py
+++ Ej 3 - Transformacion sistema solar/ej3.py
@@ -11,6 +11,7 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import grafica.basic_shapes as bs
 import grafica.easy_shaders as es
 import grafica.transformations as tr
+import math
 
 __author__ = "Daniel Calderon"
 __license__ = "MIT"
@@ -47,6 +48,44 @@ def on_key(window, key, scancode, action, mods):
         print('Unknown key')
 
 
+#--------------------PRIMER CAMBIO Crear circulos rgb ------------------
+
+class Shape:
+    def __init__(self, vertices, indices, textureFileName=None):
+        self.vertices = vertices
+        self.indices = indices
+        self.textureFileName = textureFileName
+
+#Funcion sacada de basic shapes, pero modificada dejando fijo el numero de triangulos
+#y tomando como imput el color rgb
+
+def createCircleRGB(r,g,b):
+
+    # First vertex at the center, white color
+    vertices = [0, 0, 0, 1.0, 1.0, 1.0]
+    indices = []
+
+    dtheta = 2 * math.pi / 50
+
+    for i in range(50):
+        theta = i * dtheta
+
+        vertices += [
+            # vertex coordinates
+            0.5 * math.cos(theta), 0.5 * math.sin(theta), 0,
+
+            # color generates varying between 0 and 1
+                  r,       g, b]
+
+        # A triangle is created using the center, this and the next vertex
+        indices += [0, i, i+1]
+
+    # The final triangle connects back to the second vertex
+    indices += [0, 50, 1]
+
+    return Shape(vertices, indices)
+
+
 if __name__ == "__main__":
 
     # Initialize glfw
@@ -74,16 +113,28 @@ if __name__ == "__main__":
     # Setting up the clear screen color
     glClearColor(0.15, 0.15, 0.15, 1.0)
 
-    # Creating shapes on GPU memory
-    shapeTriangle = bs.createRainbowTriangle()
-    gpuTriangle = es.GPUShape().initBuffers()
-    pipeline.setupVAO(gpuTriangle)
-    gpuTriangle.fillBuffers(shapeTriangle.vertices, shapeTriangle.indices, GL_STATIC_DRAW)
 
-    shapeQuad = bs.createRainbowQuad()
-    gpuQuad = es.GPUShape().initBuffers()
-    pipeline.setupVAO(gpuQuad)
-    gpuQuad.fillBuffers(shapeQuad.vertices, shapeQuad.indices, GL_STATIC_DRAW)
+
+    #--------------------SEGUNDO CAMBIO Creacion de los circulos en gpu---------------------------
+    #Tuve que crear tres por los colores rgb
+    #Creando el sol
+    shapeCircle = createCircleRGB(1,1,0)
+    gpuCircle = es.GPUShape().initBuffers()
+    pipeline.setupVAO(gpuCircle)
+    gpuCircle.fillBuffers(shapeCircle.vertices, shapeCircle.indices, GL_STATIC_DRAW)
+
+    #Creando la tierra
+    shapeCircle2 = createCircleRGB(0,0,1)
+    gpuCircle2 = es.GPUShape().initBuffers()
+    pipeline.setupVAO(gpuCircle2)
+    gpuCircle2.fillBuffers(shapeCircle2.vertices, shapeCircle2.indices, GL_STATIC_DRAW)
+
+    #Creando la luna
+    shapeCircle3 = createCircleRGB(1,1,1)
+    gpuCircle3 = es.GPUShape().initBuffers()
+    pipeline.setupVAO(gpuCircle3)
+    gpuCircle3.fillBuffers(shapeCircle3.vertices, shapeCircle3.indices, GL_STATIC_DRAW)
+    #----------------------------------------------------------------------------------------------------
 
     while not glfw.window_should_close(window):
         # Using GLFW to check for input events
@@ -101,53 +152,41 @@ if __name__ == "__main__":
         # Using the time as the theta parameter
         theta = glfw.get_time()
 
-        # Triangle
-        triangleTransform = tr.matmul([
-            tr.translate(0.5, 0.5, 0),
+
+
+        #-------------------- TERCER PASO CREAR TRANSFORMACIONES----------------------------------------
+        #Transformacion para el sol
+        circleTransform2 = tr.matmul([
+            tr.translate(0, 0, 0),
             tr.rotationZ(2 * theta),
             tr.uniformScale(0.5)
         ])
+        glUniformMatrix4fv(glGetUniformLocation(pipeline.shaderProgram, "transform"), 1, GL_TRUE, circleTransform2)
+        pipeline.drawCall(gpuCircle)
 
-        # updating the transform attribute
-        glUniformMatrix4fv(glGetUniformLocation(pipeline.shaderProgram, "transform"), 1, GL_TRUE, triangleTransform)
-
-        # drawing function
-        pipeline.drawCall(gpuTriangle)
-
-        # Another instance of the triangle
-        triangleTransform2 = tr.matmul([
-            tr.translate(-0.5, 0.5, 0),
-            tr.scale(
-                0.5 + 0.2 * np.cos(1.5 * theta),
-                0.5 + 0.2 * np.sin(2 * theta),
-                0)
+        #Transformacion para la tierra
+        circleTransform = tr.matmul([
+            tr.translate(0.5*np.cos(theta), 0.5*np.sin(theta), 0),
+            tr.rotationZ(2 * theta),
+            tr.uniformScale(0.2)
         ])
-        glUniformMatrix4fv(glGetUniformLocation(pipeline.shaderProgram, "transform"), 1, GL_TRUE, triangleTransform2)
-        pipeline.drawCall(gpuTriangle)
+        glUniformMatrix4fv(glGetUniformLocation(pipeline.shaderProgram, "transform"), 1, GL_TRUE, circleTransform)
+        pipeline.drawCall(gpuCircle2)
 
-        # Quad
-        quadTransform = tr.matmul([
-            tr.translate(-0.5, -0.5, 0),
-            tr.rotationZ(-theta),
-            tr.uniformScale(0.7)
+        #Transformacion para la luna
+        circleTransform3 = tr.matmul([
+            tr.translate(0.1*np.cos(theta), 0.1*np.sin(theta), 0),
+            tr.translate(0.5*np.cos(theta), 0.5*np.sin(theta), 0),
+            tr.rotationZ(2*theta),
+            tr.uniformScale(0.1)
         ])
-        glUniformMatrix4fv(glGetUniformLocation(pipeline.shaderProgram, "transform"), 1, GL_TRUE, quadTransform)
-        pipeline.drawCall(gpuQuad)
-
-        # Another instance of the Quad
-        quadTransform2 = tr.matmul([
-            tr.translate(0.5, -0.5, 0),
-            tr.shearing(0.3 * np.cos(theta), 0, 0, 0, 0, 0),
-            tr.uniformScale(0.7)
-        ])
-        glUniformMatrix4fv(glGetUniformLocation(pipeline.shaderProgram, "transform"), 1, GL_TRUE, quadTransform2)
-        pipeline.drawCall(gpuQuad)
+        glUniformMatrix4fv(glGetUniformLocation(pipeline.shaderProgram, "transform"), 1, GL_TRUE, circleTransform3)
+        pipeline.drawCall(gpuCircle3)
 
         # Once the drawing is rendered, buffers are swap so an uncomplete drawing is never seen.
         glfw.swap_buffers(window)
 
     # freeing GPU memory
-    gpuTriangle.clear()
-    gpuQuad.clear()
+    gpuCircle.clear()
     
     glfw.terminate()
