f3a6341e5e7a3f11cad1db11ecb27f2516eccc20
VicenteHS <vicente.hidalgo@ug.uchile.cl>
2021-05-13 00:56:33 -0400

    Update Tarea1.py
    Se logró  crear circulos en posiciones al azar, y numeros, falta juntarlos.

--- Tarea 1/Tarea1.py
+++ Tarea 1/Tarea1.py
@@ -10,6 +10,8 @@ import grafica.basic_shapes as bs
 import grafica.easy_shaders as es
 import grafica.transformations as tr
 import grafica.scene_graph as sg
+import random
+import grafica.text_renderer as tx
 
 ########################################################################
 # Obtener lista con los valores puestos en el csv
@@ -82,34 +84,6 @@ def mouse_button_callback(window, button, action, mods):
     elif (action ==glfw.RELEASE):
         if (button == glfw.MOUSE_BUTTON_1):
             controller.leftClickOn = False
-####################################################################
-#Creacion de figuras
-def createNodos(pipeline):
-
-    # Creacion figura gpu de circulo
-    shapeCirculo = bs.createCircleRGB(30,0.7,0.4,0.2)
-    gpuCirculo = es.GPUShape().initBuffers()
-    pipeline.setupVAO(gpuCirculo)
-    gpuCirculo.fillBuffers(shapeCirculo.vertices, shapeCirculo.indices, GL_STATIC_DRAW)
-
-    # Creacion de un Nodo
-    Nodo = sg.SceneGraphNode("Nodo")
-    Nodo.transform = tr.uniformScale(0.5)
-    Nodo.childs = gpuCirculo
-
-    # Creacion de todos los nodos de Numeros
-    Nodos = sg.SceneGraphNode("Nodos")
-    
-    for i in range(len(Numeros)):
-
-        newNode = sg.SceneGraphNode("Nodo" + str(i))
-        newNode.transform = tr.identity
-        newNode.childs += [Nodo]
-
-        Nodos.childs += [newNode]
-
-    return Nodos
-
 
 
 
@@ -120,24 +94,26 @@ def createNodos(pipeline):
 # Codigo principal
 
 if __name__ == "__main__":
-
+    ####################################################################
     # Initialize glfw
     if not glfw.init():
         glfw.set_window_should_close(window, True)
 
-    width = 600
-    height = 600
+    width = 900
+    height = 900
 
-    window = glfw.create_window(width, height, "Handling mouse events", None, None)
+    window = glfw.create_window(width, height, "ert", None, None)
 
     if not window:
         glfw.terminate()
         glfw.set_window_should_close(window, True)
 
     glfw.make_context_current(window)
-
-
-    
+    ###################################################################
+    #pipelines
+    texturePipeline = es.SimpleTextureShaderProgram()
+    textPipeline = tx.TextureTextRendererShaderProgram()
+    pipeline = es.SimpleTransformShaderProgram()
 
     ####################################################################
     # Teclado a glfw
@@ -148,35 +124,130 @@ if __name__ == "__main__":
 
     ####################################################################
     # Eleccion de pipeline
-    pipeline = es.SimpleTransformShaderProgram()
-    glUseProgram(pipeline.shaderProgram)
-
+    
+    
+    ####################################################################
+    # Enabling transparencies
+    glEnable(GL_BLEND)
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
 
     glClearColor(0.3, 0.3, 0.3, 1.0)
+    glPolygonMode(GL_FRONT_AND_BACK, GL_FILL)
+
+    #glfw.swap_interval(0)
+
+    ####################################################################
+    #Creacion de figuras
+
+    #Letras
+    # Creating texture with all characters
+    textBitsTexture = tx.generateTextBitsTexture()
+    # Moving texture to GPU memory
+    gpuText3DTexture = tx.toOpenGLTexture(textBitsTexture)
 
 
-    glfw.swap_interval(0)
 
-    Nodos = createNodos(pipeline)
+    def createCirculos(pipeline):
+
+        # Creacion figura gpu de circulo
+        shapeCirculo = bs.createCircleRGB(30,0.7,0.4,0.2)
+        gpuCirculo = es.GPUShape().initBuffers()
+        pipeline.setupVAO(gpuCirculo)
+        gpuCirculo.fillBuffers(shapeCirculo.vertices, shapeCirculo.indices, GL_STATIC_DRAW)
+
+        # Creacion de un Nodo
+        Circulo = sg.SceneGraphNode("Circulo")
+        Circulo.transform = tr.uniformScale(0.15)
+        Circulo.childs += [gpuCirculo]
+
+        # Creacion de todos los nodos de Numeros
+        Circulos = sg.SceneGraphNode("Circulos")
+        
+        for i in range(len(Numeros)):
+            x = random.uniform(-0.75,0.75)
+            y = random.uniform(-0.75,0.75)
+            newNode = sg.SceneGraphNode("Nodo" + str(i) +"trasladado")
+            newNode.transform = tr.translate(x,y,0)
+            newNode.childs += [Circulo]
+
+            Circulos.childs += [newNode]
+
+        return Circulos
+
+
+        
+    def createNumbers(Numeros):
+        gpusNumbers = []
+        for i in range(len(Numeros)):
+            headerText = str(Numeros[i])
+            headerCharSize = 0.1
+            headerCenterX = headerCharSize * len(headerText) / 2
+            headerShape = tx.textToShape(headerText, headerCharSize, headerCharSize)
+            gpuHeader = es.GPUShape().initBuffers()
+            textPipeline.setupVAO(gpuHeader)
+            gpuHeader.fillBuffers(headerShape.vertices, headerShape.indices, GL_STATIC_DRAW)
+            gpuHeader.texture = gpuText3DTexture
+            gpusNumbers.append(gpuHeader)
+        return gpusNumbers
+
+    # def createNumbers(Numeros):
+    #     Numeros = sg.SceneGraphNode("Numeros")
+
+    #     for i in range(len(Numeros)):
+    #         x = random.uniform(-0.75,0.75)
+    #         y = random.uniform(-0.75,0.75)
+
+    #         headerText = str(Numeros[i])
+    #         headerCharSize = 0.1
+    #         headerCenterX = headerCharSize * len(headerText) / 2
+    #         headerShape = tx.textToShape(headerText, headerCharSize, headerCharSize)
+    #         gpuHeader = es.GPUShape().initBuffers()
+    #         textPipeline.setupVAO(gpuHeader)
+    #         gpuHeader.fillBuffers(headerShape.vertices, headerShape.indices, GL_STATIC_DRAW)
+    #         gpuHeader.texture = gpuText3DTexture
+            
+    #         Numeros.childs += [gpuHeader]
+    #         Numeros.transform = tr.translate(x, y, 0)
+
+
+
+    gpusNumbers = createNumbers(Numeros)
+    Circulos = createCirculos(pipeline)
+
+
+
+
+
+
+
+
+
+
 
     while not glfw.window_should_close(window):
-        glfw.set_window_title(window, "hola")
         glfw.poll_events()
 
-
-        if (controller.fillPolygon):
-            glPolygonMode(GL_FRONT_AND_BACK, GL_FILL)
-        else:
-            glPolygonMode(GL_FRONT_AND_BACK, GL_LINE)
-
-        # Clearing the screen
         glClear(GL_COLOR_BUFFER_BIT)
+        glUseProgram(textPipeline.shaderProgram)
+        # for i in range (len(gpusNumbers)):
+        #     glUniform4f(glGetUniformLocation(textPipeline.shaderProgram, "fontColor"), 1,1,1,0)
+        #     glUniform4f(glGetUniformLocation(textPipeline.shaderProgram, "backColor"), 0.5,0.5,0.5,1)
+        #     glUniformMatrix4fv(glGetUniformLocation(textPipeline.shaderProgram, "transform"), 1, GL_TRUE, tr.identity())
+        #     textPipeline.drawCall(gpusNumbers[i])
 
 
-        sg.drawSceneGraphNode(Nodos, pipeline,"Transform")
+        glUniform4f(glGetUniformLocation(textPipeline.shaderProgram, "fontColor"), 1,1,1,0)
+        glUniform4f(glGetUniformLocation(textPipeline.shaderProgram, "backColor"), 0.5,0.5,0.5,1)
+        glUniformMatrix4fv(glGetUniformLocation(textPipeline.shaderProgram, "transform"), 1, GL_TRUE, tr.identity())
+        textPipeline.drawCall(gpusNumbers[9])
+
+
+
+        glUseProgram(pipeline.shaderProgram)
+        sg.drawSceneGraphNode(Circulos, pipeline,"transform")
 
         glfw.swap_buffers(window)
-    
-    #gpuCirculo.clear()
+
+    Circulos.clear()
 
     glfw.terminate()
