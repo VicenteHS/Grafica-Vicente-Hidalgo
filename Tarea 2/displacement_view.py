200cce6120c5203097d207aad8f9ac76c65ff802
VicenteHS <vicente.hidalgo@ug.uchile.cl>
2021-06-19 22:29:17 -0400

    cambio fallido

--- Tarea 2/displacement_view.py
+++ Tarea 2/displacement_view.py
@@ -308,28 +308,21 @@ class Displacement3DNoLights:
         vertex_shader = """
             #version 330
             
+            uniform mat4 projection;
+            uniform mat4 view;
+            uniform mat4 model;
+
             in vec3 position;
             in vec2 texCoords;
-            in vec3 normal;
 
-            out vec3 fragPosition;
-            out vec2 fragTexCoords;
-            out vec3 fragNormal;
-
-            uniform mat4 model;
-            uniform mat4 view;
-            uniform mat4 projection;
+            out vec2 outTexCoords;
 
             void main()
             {
-                fragPosition = vec3(model * vec4(position, 1.0));
-                fragTexCoords = texCoords;
-                fragNormal = mat3(transpose(inverse(model))) * normal;  
-                
-                gl_Position = projection * view * vec4(fragPosition, 1.0);
+                gl_Position = projection * view * model * vec4(position, 1.0f);
+                outTexCoords = texCoords;
             }
             """
-
         fragment_shader = """
             #version 330
 
